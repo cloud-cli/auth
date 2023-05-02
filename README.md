353c06af0c516533c1ca96ca10ba0e9926c6bbf9
Darlan Alves <me@darlanalv.es>
2023-05-02 14:25:26 +0000

    docs: update README

--- README.md
+++ README.md
@@ -10,3 +10,37 @@ Node.js app to perform user authentication
 | GOOGLE_CLIENT_ID      | OAuth client id                                    | true     |
 | GOOGLE_CLIENT_SECRET  | OAuth client secret                                | true     |
 | AUTH_CALLBACK_HOST    | Callback authentication host, e.g. https://foo.com | true     |
+
+## Usage
+
+Just run the Docker image:
+
+```bash
+docker run --name 'auth' --detach \
+  -e GOOGLE_CLIENT_ID='xxx' \
+  -e GOOGLE_CLIENT_SECRET='xxx' \
+  -e AUTH_CALLBACK_HOST='https://foo.com/' \
+  -e PORT=3000 ghcr.io/cloud-cli/auth:latest
+```
+
+## API
+
+*GET /profile*:
+
+Returns `{ id, displayName, photo? }`
+
+*DELETE /*:
+
+Delets the session and logs out
+
+*GET /* or *HEAD /*:
+
+Returns 200 if authenticated, 401 if not
+
+*GET /login?url=xxx*:
+
+Returns a login page. Optionally, sets up a redirection after login
+
+*GET /success*:
+
+Returns a confirmation page after login.
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import { readFileSync } from 'fs';
 import { initUser } from './user.js';
 import session from './session.js';
@@ -17,9 +17,9 @@ function protectedRoute(req, res, next) {
 }
 
 const scopes = {
-  scope: ["profile", "email"],
-  failureRedirect: "/login",
-  successRedirect: "/success",
+  scope: ['profile', 'email'],
+  failureRedirect: '/login',
+  successRedirect: '/success',
 };
 
 const app = express();
@@ -28,15 +28,20 @@ app.use(session);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.head("/", protectedRoute, (_req, res) => res.status(200).send(''));
-app.get("/", protectedRoute, (_req, res) => res.status(200).send('OK'));
-app.get("/profile", protectedRoute, (req, res) => res.send(req.user));
-app.get("/auth/google", passport.authenticate("google", scopes));
-app.get(callback, passport.authenticate("google", scopes));
-app.get("/login", (_, res) => res.send(loginPage));
-app.get("/success", (_, res) => res.send(successPage));
+app.head('/', protectedRoute, (_req, res) => res.status(200).send(''));
+app.get('/', protectedRoute, (_req, res) => res.status(200).send('OK'));
+app.delete('/', protectedRoute, (req, res, next) =>
+  req.logout((err) => (err ? next(err) : res.status(202).send('OK'))),
+);
+
+app.get('/profile', protectedRoute, (req, res) => res.send(req.user));
+app.get('/auth/google', passport.authenticate('google', scopes));
+app.get(callback, passport.authenticate('google', scopes));
+
+app.get('/login', (_, res) => res.send(loginPage));
+app.get('/success', (_, res) => res.send(successPage));
 
 app.listen(PORT, async () => {
   await initUser();
-  console.log("Auth is running on port " + PORT);
+  console.log('Auth is running on port ' + PORT);
 });
