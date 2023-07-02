0d8c0130c89764a778f918f749091609c10ccc70
Darlan Alves <me@darlanalv.es>
2023-07-02 23:55:26 +0200

    chore: update store driver

--- src/user.ts
+++ src/user.ts
@@ -1,8 +1,13 @@
-import { Model, Primary, Property, Resource, SQLiteDriver, Unique } from '@cloud-cli/store';
+import {
+  Model,
+  Primary,
+  Property,
+  Resource,
+  StoreDriver,
+  Unique,
+} from "@cloud-cli/store";
 
-const dbPath = process.env.DB_PATH;
-
-@Model('auth_user')
+@Model("auth_user")
 export class User extends Resource {
   @Unique() @Property(String) userId: string;
   @Property(Object) profile: any;
@@ -10,7 +15,7 @@ export class User extends Resource {
   @Property(String) refreshToken: string;
 }
 
-@Model('auth_property')
+@Model("auth_property")
 export class UserProperty extends Resource {
   @Primary() @Property(Number) uid: number;
   @Unique() @Property(String) userId: string;
@@ -19,7 +24,7 @@ export class UserProperty extends Resource {
 }
 
 export async function initUser() {
-  Resource.use(new SQLiteDriver(dbPath));
+  Resource.use(new StoreDriver());
   await Resource.create(User);
   await Resource.create(UserProperty);
 }
