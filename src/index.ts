0eaddafd9390b0048ae091d2350d15843deccb5c
Darlan Alves <me@darlanalv.es>
2023-07-15 17:54:35 +0200

    chore: revert cors on auth level

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import { Resource, Query } from "@cloud-cli/store";
 import { readFileSync } from "fs";
 import { UserProperty, initUser } from "./user.js";
@@ -97,7 +96,6 @@ const scopes = {
 
 const app = express();
 
-app.use(cors());
 app.use(session);
 app.use(passport.initialize());
 app.use(passport.session());
